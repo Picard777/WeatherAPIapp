93ff20b8212118519dd552696af71bc353c23eb4
Maksymilian Bzowski <max@MacBook-Air-Maks.local>
2025-12-15 19:24:37 +0100

    Add .env to gitignore

--- weatherapi.py
+++ weatherapi.py
@@ -1,7 +1,8 @@
 import json
 import requests
+import os
 
-API_KEY = "REMOVED_API_KEY"
+API_KEY= "YOUR_API_KEY"
 ONECALL_URL = "https://api.openweathermap.org/data/2.5/weather"
 GEO_URL = "http://api.openweathermap.org/geo/1.0/direct"
 
